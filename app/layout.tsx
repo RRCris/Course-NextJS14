b9cf93458ec36d27fb815542422c3bded88b1084
RRCris <curiosity.cdrr@gmail.com>
2023-11-20 19:19:14 -0500

    add font from nextjs-googlefonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { montserrat } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Vercel Course</title>
+      </head>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Montserrat } from 'next/font/google';
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '300', '500', '800'],
+});
