4a70267b2c8f805ba7c53ff56e62f968354e1bfc
RRCris <curiosity.cdrr@gmail.com>
2023-11-20 19:40:57 -0500

    add sistem routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -25,7 +26,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Screen"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+          />
+          <Image
+            src="/hero-mobile.png"
+            alt="Screen"
+            width={1000}
+            height={760}
+            className="block md:hidden"
+          />
         </div>
       </div>
     </main>
