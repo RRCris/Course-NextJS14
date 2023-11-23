5e1ff93cfc11aeb995079852ea06ecb6f0e6f583
RRCris <curiosity.cdrr@gmail.com>
2023-11-23 04:55:29 -0500

    add sistem routing && layouts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
-import { Montserrat } from 'next/font/google';
+import { Lusitana, Montserrat } from 'next/font/google';
 
 export const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['100', '300', '500', '800'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
